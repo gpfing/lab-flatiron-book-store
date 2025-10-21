0150655fc880049cb6131b1145defeeb9fef40c1
Garrett Pfingston <gpfingston@gmail.com>
2025-10-20 21:04:17 -0500

    Added different elements to the webpage

--- index.js
+++ index.js
@@ -45,3 +45,25 @@ const bookStore = {
 
 // Write your code here!
 
+const bookStoreTitle = document.querySelector("#header");
+bookStoreTitle.textContent = bookStore.name;
+
+bookStore.books.forEach(book => {
+    const bookList = document.querySelector("#book-list");
+
+    const bookContainer = document.createElement("li");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookImage = document.createElement("img");
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = `by ${book.author}`;
+    bookImage.src = book.imageUrl;
+    bookImage.alt = `Cover image of the book ${book.title}`;
+
+    bookContainer.append(bookTitle);
+    bookContainer.append(bookAuthor);
+    bookContainer.append(bookImage);
+
+    bookList.append(bookContainer);
+});
